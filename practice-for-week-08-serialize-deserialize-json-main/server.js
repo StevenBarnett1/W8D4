f0fdce31add8ddf6ec959deadc04611abc45c88b
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-07-22 10:33:44 -0700

    second

--- practice-for-week-08-serialize-deserialize-json-main/server.js
+++ practice-for-week-08-serialize-deserialize-json-main/server.js
@@ -1,3 +1,4 @@
+const { ETIMEDOUT } = require('constants');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -9,15 +10,22 @@ const server = http.createServer((req, res) => {
   });
 
   req.on("end", () => {
+    console.log("test")
+    console.log(req.headers["content-type"])
+    if(req.headers["content-type"]==="application/json"){
+      req.body = JSON.parse(reqBody)
+      console.log("Parsing json body",req.body)
+    }
+    else if(req.headers["content-type"]==="application/x-www-form-urlencoded"){
     // Parse the body of the request as JSON if Content-Type header is
       // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
       // header is x-www-form-urlencoded
     if (reqBody) {
       req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+      .split("&")
+      .map((keyValuePair) => keyValuePair.split("="))
+      .map(([key, value]) => [key, value.replace(/\+/g, " ")])
         .map(([key, value]) => [key, decodeURIComponent(value)])
         .reduce((acc, [key, value]) => {
           acc[key] = value;
@@ -25,8 +33,8 @@ const server = http.createServer((req, res) => {
         }, {});
 
       // Log the body of the request to the terminal
-      console.log(req.body);
-    }
+      console.log("Parsing percent encoded body",req.body)
+    }}
 
     const resBody = {
       "Hello": "World!"
@@ -34,8 +42,41 @@ const server = http.createServer((req, res) => {
 
     // Return the `resBody` object as JSON in the body of the response
   });
+
+
+  if(req.url === "/posts/:id" && req.method === "GET"){
+
+  }
+
+  if(req.url === "/posts" && req.method === "POST"){
+
+  }
+
+  if(req.url === "/posts/:id" && req.method === "PATCH"){}
+
+
+  if(req.url === "/users" && req.method === "POST"){}
+
+  if(req.url === "/posts/comments" && req.method === "GET"){}
+
+  if(req.url === "posts/:id/comments" && req.method === "POST"){}
+
+  if(req.url === "posts/:postId/comments/:commentId" && req.method === "PATCH")
+
+  if(req.url === "posts/:postId/comments/:commentId" && req.method === "DELETE")
+
+  if(req.url === "posts/:postId" && req.method === "PUT"){}
+
+
+
+
+
+
+
+
+
 });
 
-const port = 5000;
+const port = 3000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
